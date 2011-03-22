46914886dad4f59b279b1f942b8a3ca7da2aedcc
HairyFotr <maticpotocnik@gmail.com>
2011-03-22 19:56:36 +0100

    uredil in dodal zapiske

--- PPJ/list.pl
+++ PPJ/list.pl
@@ -1,33 +1,17 @@
 %te funkcije so "standard" in jih bomo redno uporabljali
 
-member1(X,L) :-
-	L=[H|T],
-	(X=H;
-	 member1(X,T)).
+member1(X,[X|_]).
+member1(X,[_|T]) :-
+	 member1(X,T).
 
-conc(L1,L2,L) :-
-    L1=[], L=L2.
-conc(L1,L2,L) :-
-    L1=[L1H|L1T],
-    conc(L1T,L2,LL),
-    L=[L1H|LL].
-%skrajšana verzija... *kjer je = se verjetno da
-%append([], Ys, Ys).
-%append([X|Xs], Ys, [X|Zs]) :- append(Xs, Ys, Zs).
+conc([],L,L).
+conc([L1H|L1T],L2,[L1H|L2T]) :-
+    conc(L1T,L2,L2T).
 
-insert(X,L,L1) :-
-    L1=[X|L].
-insert(X,L,[H|LL]) :-
-	L=[H|T],
-	insert(X,T,LL),
+insert(X,L,[X|L]).
+insert(X,[H|T],[H|LL]) :-
+	insert(X,T,LL).
 	    
-delete(X,L,L1) :-
-    L=[H|T],
-    X=H, L1=T.
-delete(X,L,[H|LL]) :-
-    L=[H|T],
-    %\+ H=X,
-    delete(X,T,LL),
-    
-    
-
+delete(X,[X|T],T).
+delete(X,[H|T],[H|LL]) :-
+    delete(X,T,LL).
